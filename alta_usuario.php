c9c51ef4441a0ed52c49ee8a6e4b5b25a5755c99
aaron agullo sanchez <aaronagullo24@gmail.com>
2019-11-06 12:30:34 +0100

    arreglosç

--- alta_usuario.php
+++ alta_usuario.php
@@ -1,3 +1,39 @@
+
+<?php
+include "funciones.php";
+boton();
+$Nombre="";
+$contraseña="";
+if (isset($_POST['Nombre'])) {
+    $Nombre = $_POST['Nombre'];
+    $contraseña = $_POST['contraseña'];
+   
+    $correcto = false;
+
+    $fichero = fopen("usuarios.txt", "r");
+    while (($linea = fgets($fichero)) && !$encontrado) {
+        $linea = trim($linea);
+        $posicion_espacio = strpos($linea, " ");
+        $nombre = substrt($linea, 0, $posicion_espacio);
+
+        $longitud_pass = strlen($linea) - ($posicion_espacio + 1);
+
+        $pass = trim($pass);
+        $pass = substr($linea, $posicion_espacio + 1, $longitud_pass);
+
+        if ($usu == $nombre && $contraseña == $contraseña) $encontrado = true;
+    }
+    fclose($fichero);
+
+    if ($encontrado && !$error) {
+        $file = fopen("usuarios.txt", "w");
+        fwrite($file, $Nombre . PHP_EOL);
+
+        fwrite($file,$contraseña . PHP_EOL);
+        fclose($file);
+    } else echo "USUARIO NO VALIDO";
+}
+?>
 <html>
 
 <head>
@@ -12,27 +48,8 @@
         <input type="submit" value="dar de alta" name="ver" id="ver todo">
     </form>
 
-    
-    
+
+
 </body>
 
 </html>
-<?php
-    include "funciones.php";
-    boton();
-    if (isset($_POST['Nombre'])) {
-        $Nombre = $_POST['Nombre'];
-        $contraseña = $_POST['contraseña'];
-
-        
-
-
-    $file = fopen("usuarios.txt", "w");
-
-    fwrite($file, "Esto es una nueva linea de texto" . PHP_EOL);
-
-    fwrite($file, "Otra más" . PHP_EOL);
-
-    fclose($file);
-    }
-    ?>
